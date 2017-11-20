8cd1c281c56d83638e3c5735fcf8d232de21bcae
ECRSSS <n.n.glebanov@gmail.com>
2017-11-20 15:22:10 +0300

    Собранный проект без обфускации

---
+++ lesson1/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Created by Administrator on 20.11.2017.
+ */
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Hello Otus!");
+        WebDriver driver=new ChromeDriver();
+    }
+}
